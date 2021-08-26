a90d417c02117544121dfb97a2dd8bbb6819e43e
Dhia7 <88534315+Dhia7@users.noreply.github.com>
2021-08-26 13:03:25 +0200

    Add files via upload

---
+++ Testtt/js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+    numb: 1,
+    question: "What is arguably the most famous EDM festival in North America?",
+    answer: "Ultra",
+    options: [
+      "Ultra",
+      "Coachella",
+      "Sonar",
+      "Tomorrowland"
+    ]
+  },
+  {
+    numb: 2,
+    question: "New Orleans Jazz Fest is one of the oldest music festivals around. When was the first Jazz Fest?",
+    answer: "1970",
+    options: [
+      "1982",
+      "1958",
+      "1970",
+      "1971"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which California-based music festival tends to make news for its celebrity sightings as much as its musical lineup?",
+    answer: "Coachella",
+    options: [
+      "Lollapalooza",
+      "Burning Man",
+      "Coachella",
+      "Electric Forest"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Where is the Roskilde Music Festival held?",
+    answer: "Denmark",
+    options: [
+      "Germany",
+      "Finland",
+      "Nevada",
+      "Denmark"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the largest music festival in the United States?",
+    answer: "Summerfest",
+    options: [
+      "Coachella",
+      "Summerfest",
+      "Lollapalooza",
+      "Lost Lands"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
